ba34c0f06262603d74d2970daf91545717f88532
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2023-11-30 23:39:06 -0500

    adding routes to redirect user to other pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,51 @@
 
-import React, {useEffect, useState} from 'react'
-import './App.css'
-import Header from './components/Header'
-import MiddleSection from './components/MiddleSection';
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import SignupPage from './components/SignupPage';
+import Header from './components/Header';
+import SearchSection from './components/SearchSection';
 import CreateListSection from './components/CreateListSection';
 import AddToListSection from './components/AddToListSection';
 import DeleteListSection from './components/DeleteListSection';
-
-import SideNav from './components/SideNav'
+import SideNav from './components/SideNav';
+import Menu from './components/Menu';
 
 function App() {
-
-  const [backendData, setBackendData] = useState([{}])
-
-  useEffect(() => { 
-    fetch('/api/superheroInfo')
-      .then(res => res.json())
-      .then(data => setBackendData(data))
-  }, [])
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    localStorage.getItem('isAuthenticated') === 'true'
+  );
 
   return (
-    <div>
-      <SideNav />
-      <Header />
-      <MiddleSection />
-      <AddToListSection />
-      <CreateListSection />
-      <DeleteListSection />
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Menu isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/login" element={<LoginPage isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/signup" element={<SignupPage isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+        <Route
+          path="/dashboard/*"
+          element={
+            isAuthenticated ? (
+              <>
+                <SideNav setIsAuthenticated={setIsAuthenticated} />
+                <div className="content">
+                  <Header setIsAuthenticated={setIsAuthenticated} />
+                  <SearchSection setIsAuthenticated={setIsAuthenticated} />
+                  <CreateListSection setIsAuthenticated={setIsAuthenticated} />
+                  <AddToListSection setIsAuthenticated={setIsAuthenticated} />
+                  <DeleteListSection setIsAuthenticated={setIsAuthenticated} />
+                </div>
+              </>
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
+        <Route path="/*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
