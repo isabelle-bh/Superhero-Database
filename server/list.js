04855592cf2f7299bcba4d11b851c9d409a6a151
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2023-12-03 20:16:18 -0500

    adding list attributes to mongo schema

--- server/list.js
+++ server/list.js
@@ -4,6 +4,16 @@ const mongoose = require('./db'); // Import your MongoDB connection
 const listSchema = new mongoose.Schema({
   name: String,
   superheroes: [String], // Change the data type as needed
+  user: { type: String, required: true }, // Change the type to String
+  desc: String,
+  visibility: String,
+  username: String,
+  rating: {
+    type: Number,
+    min: 1,
+    max: 10
+  },
+  comments: [String],
 });
 
 const List = mongoose.model('List', listSchema);
