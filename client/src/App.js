b23e0b61a9fb13a9bd784b137b02a6286fe1bb6b
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2023-12-03 20:16:46 -0500

    adjusting paths in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
@@ -9,18 +8,24 @@ import SearchSection from './components/SearchSection';
 import CreateListSection from './components/CreateListSection';
 import AddToListSection from './components/AddToListSection';
 import DeleteListSection from './components/DeleteListSection';
+import Admin from './components/Admin';
 import SideNav from './components/SideNav';
 import Menu from './components/Menu';
 
 function App() {
+  // Initially set isAuthenticated based on localStorage
   const [isAuthenticated, setIsAuthenticated] = useState(
     localStorage.getItem('isAuthenticated') === 'true'
   );
 
+  useEffect(() => {
+    // You can perform additional setup or checks here
+  }, []);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Menu isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/" element={<Menu isAuthenticated={isAuthenticated} />} />
         <Route path="/login" element={<LoginPage isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
         <Route path="/signup" element={<SignupPage isAuthenticated={isAuthenticated} setIsAuthenticated={setIsAuthenticated} />} />
         <Route
@@ -30,7 +35,7 @@ function App() {
               <>
                 <SideNav setIsAuthenticated={setIsAuthenticated} />
                 <div className="content">
-                  <Header setIsAuthenticated={setIsAuthenticated} />
+                  <Header setIsAuthenticated={setIsAuthenticated} isAuthenticated={isAuthenticated} />
                   <SearchSection setIsAuthenticated={setIsAuthenticated} />
                   <CreateListSection setIsAuthenticated={setIsAuthenticated} />
                   <AddToListSection setIsAuthenticated={setIsAuthenticated} />
@@ -38,8 +43,29 @@ function App() {
                 </div>
               </>
             ) : (
+              // Redirect to login if not authenticated
               <Navigate to="/login" />
             )
+          }
+        />
+        <Route
+          path="/unauthorized-dashboard"
+          element={
+            // Unauthorized dashboard (only header and search section)
+            <div className="content">
+              <SideNav setIsAuthenticated={setIsAuthenticated} />
+              <Header setIsAuthenticated={setIsAuthenticated} isAuthenticated={isAuthenticated} />
+              <SearchSection setIsAuthenticated={setIsAuthenticated} />
+            </div>
+          }
+        />
+        <Route
+          path="/admin-dashboard"
+          element={
+            // Unauthorized dashboard (only header and search section)
+            <div className="content">
+              <Admin setIsAuthenticated={setIsAuthenticated} isAuthenticated={isAuthenticated} />
+            </div>
           }
         />
         <Route path="/*" element={<Navigate to="/login" />} />
